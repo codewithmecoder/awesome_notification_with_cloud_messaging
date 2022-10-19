f256af63848f27d70a82a6a695acde8848f69b24
cheanith2909 <sokcheanith1031@hotmail.com>
2022-10-19 14:06:36 +0700

    add backend

---
+++ backend/README.md
@@ -0,0 +1,4 @@
+## Getting Started
+
+You have to go to your firebase console in your project that your made for your mobile. Beside your `Project Overview` has the setting Icon click on it then click on `Project Setting` then click on `Service accounts` tap.
+After that find the button `Generate new private key` and name it `private_key.json` input it the project level. You good to go.
---
+++ backend/test-push-notification-fcm/test-push-notification-fcm/Controllers/PushNotificationFCMController.cs
@@ -0,0 +1,32 @@
+﻿using FirebaseAdmin.Messaging;
+using Microsoft.AspNetCore.Mvc;
+
+namespace test_push_notification_fcm.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class PushNotificationFCMController : ControllerBase
+{
+
+    [HttpPost]
+    public async Task<ActionResult> PushNotification()
+    {
+        var registrationToken = "fgfOPRTyS-CquiSFmsI7_k:APA91bGrLvRVRkNyaWucMS5487EeM7M9lIxepOgdkT9ETLVXIChAQBOj2qqC1MBbYSaQ-PXpxbVe7ck2Id_-cUvfoV4YYsPCSqWSe92PNEHCDXQfAx0Y04rwwgiAG8oKUAq7f1RRT1j1";
+        var message = new Message()
+        {
+            Data = new Dictionary<string, string>()
+            {
+                { "score", "850" },
+                { "time", "2:45" },
+            },
+            Token = registrationToken,
+        };
+
+        // Send a message to the device corresponding to the provided
+        // registration token.
+        string response = await FirebaseMessaging.DefaultInstance.SendAsync(message);
+        // Response is a message ID string.
+        Console.WriteLine("Successfully sent message: " + response);
+        return Ok(response);
+    }
+}
